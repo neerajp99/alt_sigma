10143f26a10dc59d4974c465ff9a55ec2423d540
neerajp99 <neerajp1999@gmail.com>
2021-08-02 22:12:06 +0530

    Update methods to log and fetch from google sheets

--- recordSheets.js
+++ recordSheets.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-const { authorize } = require("./gmail.js");
-const content = fs.readFileSync("credentials.json");
 
 /**
  * Fetched the data from Google sheets
@@ -10,22 +8,35 @@ const content = fs.readFileSync("credentials.json");
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
 */
 async function getSheetsData(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1B8upVnuawfWfcqgFlTIhkYv7yDpJw3IT2xRUs1i9zYg',
-    range: 'Data!A2:E',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Request, Name:');
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[1]}`);
-      });
-    } else {
-      console.log('No data found.');
-    }
-  });
+  return new Promise((resolve, reject) => {
+    const sheets = google.sheets({version: 'v4', auth});
+    sheets.spreadsheets.values.get({
+      spreadsheetId: '1B8upVnuawfWfcqgFlTIhkYv7yDpJw3IT2xRUs1i9zYg',
+      range: 'Data!A2:E',
+    }, 
+    (err, res) => {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        reject(err)
+      }
+
+      if (Object.keys(res.data).length > 2) {
+        const rows = res.data.values;
+        if (rows.length) {
+          // rows.map((row) => {
+          //   console.log(`${row[0]}, ${row[1]}`);
+          // });
+          resolve(rows)
+        } else {
+          console.log('No data found.');
+          resolve([])
+        }
+      } else {
+        resolve([])
+      }
+    });
+})
+
 }
 
 /**
@@ -33,12 +44,13 @@ async function getSheetsData(auth) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @return {boolean} bool Boolean value or error value
  */
-async function writeToSheets(auth) {
+async function writeToSheets(auth, email, name) {
+  return new Promise((resolve, reject) => {
     const sheets = google.sheets({ version: 'v4', auth });
     let values = [
         [
-            'Jason',
-            'Waghawale'
+            email,
+            name
         ],
     ];
     const resource = {
@@ -49,19 +61,19 @@ async function writeToSheets(auth) {
         range: 'Data!A1',
         valueInputOption: 'RAW',
         resource: resource,
-    }, (err, result) => {
+    }, (error, result) => {
         if (error) {
             console.log("Error", error);
-            return error;
+            reject(error)
         } else {
             console.log('%d cells updated on range: %s', result.data.updates.updatedCells, result.data.updates.updatedRange);
-            return true;
+            resolve(true)
         }
     });
+  })
 }
 
-(async () => {
-    const oAuth2Client = await authorize(JSON.parse(content), "tokens.json");
-    await getSheetsData(oAuth2Client)
-    await writeToSheets(oAuth2Client)
-})();
+module.exports = {
+  getSheetsData,
+  writeToSheets
+}
