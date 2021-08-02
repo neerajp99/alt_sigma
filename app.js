6b1224dc64365fa3ffb426a445f3019fb1a7940d
neerajp99 <neerajp1999@gmail.com>
2021-08-02 22:10:50 +0530

    Temp edits

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const config = require('./config');
 const {automate_fb} = require('./fb_group')
+const {automate_gmail} = require('./gmail')
 
 async function fetchRequests() {
     const result = await automate_fb()
     console.log(result);
     return result
 }
-fetchRequests()
+
+async function fetchEmails() {
+    const emails = await automate_gmail()
+    // console.log('EMAILS', emails)
+    return emails
+}
+
+// fetchRequests()
+//     .then(result => {
+//     console.log('REEEE', result)
+//     console.log('ACHSAAA')
+//     })
+//     .catch(error => {
+//         console.log('Error', error)
+//     })
+
+fetchEmails()
     .then(result => {
-    console.log('REEEE', result)
-    console.log('ACHSAAA')
+        // console.log('CHECCCC', result)
+        console.log('achsa')
     })
-    .catch(error => {
-        console.log('Error', error)
+    .catch(err => {
+        console.log('ERROR', err)
     })
 
 
